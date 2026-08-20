b74dd63aa2100645e51da1605f81340305f685c6
Luiz Felipe <luizfelipesmaciel@gmail.com>
2026-08-19 22:59:56 -0300

    Complete state machine with mocks

---
+++ app/go.mod
@@ -0,0 +1,3 @@
+module github.com/luizok/step-function-activity
+
+go 1.25.0
--- states.tf
+++ states.tf
@@ -3,7 +3,7 @@ locals {
     (
         $fmt:='[Y]-[M01]-[D01]';
         $today:=$data_movimento;
-        [0..23].(
+        [0..20].(
             $padHour:=$substring('0' & $, -2);
             $dt:=$toMillis($today&'T'&$padHour&':05:00');
             [{
@@ -35,9 +35,49 @@ resource "aws_sfn_state_machine" "this" {
       }
 
       "Generate Next Processing Dates" = {
-        Type   = "Pass"
-        Output = "{% ${local.generateDatesSingleLine} %}"
-        End    = true
+        Type = "Pass"
+        Output = {
+          executions = "{% ${local.generateDatesSingleLine} %}"
+        }
+        Next = "Proccess Data"
+      }
+
+      "Proccess Data" = {
+        Type  = "Map"
+        Items = "{% $states.input.executions %}"
+        ItemProcessor = {
+          ProcessorConfig = {
+            Mode = "INLINE"
+          }
+          StartAt = "Wait For Start Datetime"
+          States = {
+            "Wait For Start Datetime" = {
+              Type      = "Wait"
+              Timestamp = "{% $states.input.exec & '-03:00' %}"
+              Next      = "Init Hourly Processing"
+            }
+            "Init Hourly Processing" = {
+              Type = "Pass"
+              Output = {
+                data_movimento = "{% $data_movimento %}"
+                src_partition  = "{% $states.input.src_ptt %}"
+                dst_partition  = "{% $states.input.dst_ptt %}"
+                job_run_id     = "{% $uuid() %}"
+              }
+              End = true
+            }
+          }
+        }
+        Next = "Consolidate Results"
+      }
+
+      "Consolidate Results" = {
+        Type = "Pass"
+        Output = {
+          processed_partition = "{% 'data_movimento=' & $data_movimento & '/' %}"
+          job_run_id          = "{% $uuid() %}"
+        }
+        End = true
       }
     }
   })
